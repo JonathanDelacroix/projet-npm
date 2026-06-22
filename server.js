907f510c09998ef8dfa646d42e4ee67941dbac98
delacroix <delacroix.jon@gmail.com>
2026-06-22 15:09:51 +0200

    Correction activité 7

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const app = require("./app.js");
+require('dotenv').config();
+
+const app = require(".app.js");
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
