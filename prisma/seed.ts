537891a4b308eb66544e87160ff2b47265678135
delacroix <delacroix.jon@gmail.com>
2026-06-23 11:41:48 +0200

    Correction activité 8

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,58 +1,63 @@
-import { PrismaClient } from "../generated/prisma";
-
 import { prisma } from "../app";
 
-const woods = [
-  {
-    name: "Épicéa",
-    type: "softwood",
-    hardness: "tender",
-  },
-  {
-    name: "Pin",
-    type: "softwood",
-    hardness: "medium_hard",
-  },
-  {
-    name: "Padouk",
-    type: "exotic_wood",
-    hardness: "hard",
-  },
-  {
-    name: "Érable",
-    type: "noble_and_hardwoods",
-    hardness: "medium_hard",
-  },
-  {
-    name: "Hêtre",
-    type: "noble_and_hardwoods",
-    hardness: "medium_hard",
-  },
-  {
-    name: "Itauba",
-    type: "exotic_wood",
-    hardness: "hard",
-  },
-  {
-    name: "Douglas",
-    type: "softwood",
-    hardness: "tender",
-  },
-];
-
 async function main() {
-  await prisma.wood.createMany({
-    data: woods,
-    skipDuplicates: true,
-  });
+    // User
+    await prisma.user.upsert({
+        where: {
+            email: "john.doe@mail.com",
+        },
+        update: {}, // Ne rien changer si l'utilisateur existe déjà
+        create: {
+            firstName: "John",
+            lastName: "Doe",
+            email: "john.doe@mail.com",
+            password: "123456",
+        }
+    });
+
+    // Woods
+    await prisma.wood.createMany({
+        data: [
+            {
+                name: "Épicéa",
+                type: "softwood",
+                hardness: "tender",
+            },
+            {
+                name: "Pin",
+                type: "softwood",
+                hardness: "medium_hard",
+            },
+            {
+                name: "Padouk",
+                type: "exotic_wood",
+                hardness: "hard",
+            },
+            {
+                name: "Érable",
+                type: "noble_and_hardwoods",
+                hardness: "medium_hard",
+            },
+            {
+                name: "Hêtre",
+                type: "noble_and_hardwoods",
+                hardness: "medium_hard",
+            },
+            {
+                name: "Itauba",
+                type: "exotic_wood",
+                hardness: "hard",
+            },
+            {
+                name: "Douglas",
+                type: "softwood",
+                hardness: "tender",
+            }
+        ],
+        skipDuplicates: true
+    })
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    .catch((e) => console.error(e))
+    .finally(async () => await prisma.$disconnect());
