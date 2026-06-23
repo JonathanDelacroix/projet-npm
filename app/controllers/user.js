952eb16587ca1c9317a7df7a80ee69491bc7dd27
delacroix <delacroix.jon@gmail.com>
2026-06-23 17:00:39 +0200

    Activité 12

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,15 @@
-export const signup = (req, res) => {
-    res.send('You are signup');
+import { prisma } from "../../app.js";
+
+export const signup = async (req, res) => {
+    try {
+        const user = await prisma.user.create({
+            data: req.body
+        });
+
+        res.send(user);
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 export const login = (req, res) => {
