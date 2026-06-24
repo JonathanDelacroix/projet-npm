cf3df8af9ee7839bcdea3865e194b06dccfc7d9c
delacroix <delacroix.jon@gmail.com>
2026-06-24 14:08:53 +0200

    Activité 16

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,30 +1,51 @@
+import jwt from "jsonwebtoken";
+import { promisify } from "node:util";
 import bcrypt from "bcrypt";
 import { prisma } from "../../app.js";
 
-export const signup = async (req, res) => {
+const signAsync = promisify(jwt.sign);
+
+export const login = async (req, res) => {
     try {
-            const { password, ...userData } = req.body;
+        const { email, password } = req.body;
 
-            const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
 
-            const user = await prisma.user.create({
-                data: {
-                    ...userData,
-                    password: hashedPassword,
-                },
+        if (!user) {
+            return res.status(401).json({
+                error: "Invalid credentials",
             });
+        }
 
-            return res.status(201).json(user);
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            return res.status(401).json({
+                error: "Invalid credentials",
+            });
+        }
+
+        const token = await signAsync(
+            { id: user.id },
+            process.env.TOKEN_SECRET,
+            { expiresIn: Number(process.env.JWT_EXPIRATION) }
+        );
+
+        return res.status(200).json({
+            user: {
+                id: user.id,
+                email: user.email,
+            },
+            token,
+        });
 
     } catch (error) {
         console.error(error);
 
         return res.status(500).json({
-            error: error.message ?? "An error occured",
+            error: error.message ?? "An error occurred",
         });
     }
-};
-
-export const login = (req, res) => {
-    res.send('You are login');
 };
