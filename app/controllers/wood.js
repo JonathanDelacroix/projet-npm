ede6be8a6443c8df608654daa4d7234539721826
delacroix <delacroix.jon@gmail.com>
2026-06-24 11:33:57 +0200

    Activité 14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,13 @@
-export const getAll = (req, res) => {
-    res.send('List of woods');
+import { prisma } from "../../app.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const wood = await prisma.wood.findMany();
+
+    return res.status(200).json(wood);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
 };
