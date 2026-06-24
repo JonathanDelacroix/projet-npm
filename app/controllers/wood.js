51434d24634e5fcd09bd817fed3b1d81db17ad4a
delacroix <delacroix.jon@gmail.com>
2026-06-24 11:37:00 +0200

    Correction activité 14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -7,7 +7,7 @@ export const getAll = async (req, res) => {
     return res.status(200).json(wood);
   } catch (error) {
     return res.status(500).json({
-      error: error.message,
+      error: error.message ?? "An error occured",
     });
   }
 };
