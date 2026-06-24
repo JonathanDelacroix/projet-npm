9338304cef320b1c84a18f44c310a724ef5bb430
delacroix <delacroix.jon@gmail.com>
2026-06-24 15:04:15 +0200

    Correction activité 16

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,6 +5,30 @@ import { prisma } from "../../app.js";
 
 const signAsync = promisify(jwt.sign);
 
+export const signup = async (req, res) => {
+    try {
+        const { password, ...userData } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                ...userData,
+                password: hashedPassword,
+            },
+        });
+
+        return res.status(201).json(user);
+
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            error: error.message ?? "An error occured",
+        });
+    }
+};
+
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
