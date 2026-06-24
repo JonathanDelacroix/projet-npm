daf84420292a03314e48fe5a663a494ba63f5585
delacroix <delacroix.jon@gmail.com>
2026-06-24 10:00:46 +0200

    Correction activité 12

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,13 +2,19 @@ import { prisma } from "../../app.js";
 
 export const signup = async (req, res) => {
     try {
-        const user = await prisma.user.create({
-            data: req.body
-        });
+    const user = await prisma.user.create({
+        data: req.body,
+    });
 
-        res.send(user);
+    return res.status(201).json(user);
+
     } catch (error) {
-        res.send(error);
+    console.error(error);
+
+    return res.status(500).json({
+        message: "Erreur lors de la création de l'utilisateur",
+        error: error.message ?? "Erreur inconnue",
+    });
     }
 };
 
